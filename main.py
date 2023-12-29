a4fd9d507b117cf60b5d82cc92f1befbbde964a1
Kuba Marchut <kuba.marchut@gmail.com>
2023-12-29 13:59:59 +0100

    improving user output

--- main.py
+++ main.py
@@ -8,29 +8,39 @@ from modules.icmp_gen import *
 from modules.telnet_gen import *
 
 generate_traffic_functions = [
-    generate_ftp_traffic, 
-    generate_http_traffic, 
-    generate_icmp_traffic, 
-    generate_telnet_traffic
+    {"func": generate_ftp_traffic, "label": "generating ftp traffic"}, 
+    {"func": generate_http_traffic, "label": "generating http traffic"}, 
+    {"func": generate_icmp_traffic, "label": "generating icmp traffic"}, 
+    {"func": generate_telnet_traffic, "label": "generating telnet traffic"}
 ]
 
-def main_part(thread_time_span):
+def main_part(thread_time_span, dstIP):
     start_time = time.time()
     while True:
         elapsed_time = time.time() - start_time
         if elapsed_time >= thread_time_span:  # Check if n seconds have elapsed
             break
-        
+
         random_function = random.choice(generate_traffic_functions)
-        print(random_function.__name__)
-        random_function(dstIP)
+        print(random_function["label"])
+        random_function["func"](dstIP)
         time.sleep(0.1)
 
-def run_traffic_simultaneously(threads_count, thread_time_span):
+def run_traffic_simultaneously(threads_count, thread_time_span, dstIP):
+    print("Starting network traffic generator")
+    print("destination IP:", dstIP)
+    print(f"starting {threads_count} threads")
+    print(f"each thread will run for {thread_time_span}s\n")
+
+    time.sleep(1)
+
     threads = []
 
     for i in range(threads_count):
-        thread = threading.Thread(target=main_part, args=(thread_time_span))
+        thread = threading.Thread(
+            target=main_part,
+            args=(thread_time_span, dstIP)
+        )
         threads.append(thread)
         thread.start()
 
@@ -40,6 +50,10 @@ def run_traffic_simultaneously(threads_count, thread_time_span):
 
 if __name__ == "__main__":
     dstIP = "192.168.0.105"
-    threads_count = 50
+    threads_count = 1
     thread_time_span = 10
-    run_traffic_simultaneously(threads_count, thread_time_span)
+    run_traffic_simultaneously(
+        threads_count, 
+        thread_time_span, 
+        dstIP
+    )
