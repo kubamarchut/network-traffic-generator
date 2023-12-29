faf7333b7a7b894bd55df8fc59c392dc3bac040a
Kuba Marchut <kuba.marchut@gmail.com>
2023-12-29 13:40:55 +0100

    creating if main name block

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from modules.http_gen import *
 from modules.icmp_gen import *
 from modules.telnet_gen import *
 
-dstIP = "192.168.0.105"
-
 generate_traffic_functions = [generate_ftp_traffic, generate_http_traffic, generate_icmp_traffic, generate_telnet_traffic]
+
 def main_part():
     start_time = time.time()
     while True:
@@ -21,13 +20,19 @@ def main_part():
         generate_traffic_functions[random_function](dstIP)
         time.sleep(0.1)
 
-threads = []
+def run_traffic_simultaneously(threads_count):
+    threads = []
 
-for i in range(50):
-    thread = threading.Thread(target=main_part)
-    threads.append(thread)
-    thread.start()
+    for i in range(threads_count):
+        thread = threading.Thread(target=main_part)
+        threads.append(thread)
+        thread.start()
 
-# Wait for all threads to complete
-for thread in threads:
-    thread.join()
+    # Wait for all threads to complete
+    for thread in threads:
+        thread.join()
+
+if __name__ == "__main__":
+    dstIP = "192.168.0.105"
+    threads_count = 50
+    run_traffic_simultaneously(threads_count)
